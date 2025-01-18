a5547f04e2f26282070e9b92ec357da04e85c446
Vasyl Petrov <Vasyl.petrov.personal@ukr.net>
2025-01-18 20:33:39 +0100

    ddd

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-let number = 10;
-console.log('log');
-console.warn('warn');
-console.error('error');
+// undefined, null;
+
+confirm();
+prompt();
+alert();
+
+var variable = 10;
+
+const variableConst = 'Hello';
+
+let promptData; // undefined;
+
+// let confirmData = confirm('Today is Thu?');
+
+// console.log(confirmData);
+
+// console.log('before set promptData', promptData);
+
+// promptData = prompt('How are you?');
+
+/* 
+
+console.log(promptData);
+
+typeof 4;
+
+*/
+
+let num = 9.1; // number
+let str = 'hello'; // string 
+
+let bool = false; // boolean
+let bool_1 = true; // boolean
+
+let nulleble = null; // boolean 
+let und = undefined; // boolean
+
+let bigIntNum = 2n; // BigInt 
+
+// let symb = Symbol(''); // symbol
+
+num = num + 10;
+
+/// 
+let arr = [1, 2, 3, 4, 88]; 
+
+let obj = {
+    name: 'Vasyl',
+    old: 33
+};
